a1cac253b46b7366ca28d9799093e7ec2c7035c9
root <root@localhost.localdomain>
2019-08-29 05:01:53 +0800

    update

---
+++ other/hazelcast-oss/start-hazelcast.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -euo pipefail
+
+#if [ -n "${MANCENTER_URL}" ]; then
+#  sed -i "10 s#false#true#" ${HZ_HOME}/bin/hazelcast.xml
+#  sed -i "10 s#{hazelcast.mancenter.url}#$MANCENTER_URL#" ${HZ_HOME}/bin/hazelcast.xml
+#fi
+# cluster
+if [ -n "${CLUSTER_NET}" ]
+  sed -i "s/{trusted.net}/$CLUSTER_NET/" ${HZ_HOME}/bin/hazelcast.xml
+else
+  sed -i "s/{trusted.net}/127.0.0.1/" ${HZ_HOME}/bin/hazelcast.xml
+fi
+
+eval JAVA_OPTS=\"${JAVA_OPTS}\"
+eval CLASSPATH=\"${CLASSPATH}\"
+
+if [ -n "${CLASSPATH}" ]; then 
+  export CLASSPATH="${CLASSPATH_DEFAULT}:${CLASSPATH}"
+else
+  export CLASSPATH="${CLASSPATH_DEFAULT}"
+fi
+
+if [ -n "${JAVA_OPTS}" ]; then
+  export JAVA_OPTS="${JAVA_OPTS_DEFAULT} ${JAVA_OPTS}"
+else
+  export JAVA_OPTS="${JAVA_OPTS_DEFAULT}"
+fi
+
+if [ -n "${MIN_HEAP_SIZE}" ]; then
+  export JAVA_OPTS="-Xms${MIN_HEAP_SIZE} ${JAVA_OPTS}"
+fi
+
+if [ -n "${MAX_HEAP_SIZE}" ]; then
+  export JAVA_OPTS="-Xmx${MAX_HEAP_SIZE} ${JAVA_OPTS}"
+fi
+
+if [ -n "${MANCENTER_URL}" ]; then
+  export JAVA_OPTS="-Dhazelcast.mancenter.enabled=true -Dhazelcast.mancenter.url=${MANCENTER_URL} ${JAVA_OPTS}"
+else
+  export JAVA_OPTS="-Dhazelcast.mancenter.enabled=false ${JAVA_OPTS}"
+fi
+
+if [ -n "${PROMETHEUS_PORT}" ]; then
+  export JAVA_OPTS="-javaagent:${HZ_HOME}/lib/jmx_prometheus_javaagent.jar=${PROMETHEUS_PORT}:${PROMETHEUS_CONFIG} ${JAVA_OPTS}"
+fi
+
+if [ -n "${LOGGING_LEVEL}" ]; then
+  sed -i "s/java.util.logging.ConsoleHandler.level = INFO/java.util.logging.ConsoleHandler.level = ${LOGGING_LEVEL}/g" logging.properties
+  sed -i "s/.level= INFO/.level= ${LOGGING_LEVEL}/g" logging.properties
+fi
+
+echo "########################################"
+echo "# JAVA_OPTS=${JAVA_OPTS}"
+echo "# CLASSPATH=${CLASSPATH}"
+echo "# starting now...."
+echo "########################################"
+set -x
+exec java -server ${JAVA_OPTS} com.hazelcast.core.server.StartServer
