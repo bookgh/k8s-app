8066d9206193567ec318c23cdf49836792ee3dc8
root <you@example.com>
2019-09-06 20:42:14 +0800

    update

--- fdfs/fdfsOK.sh
+++ fdfs/fdfsOK.sh
@@ -2,7 +2,7 @@
 
 # 容器启动后检测如果成功继续启动下一个副本，否则下一个副本不会启动
 function status(){
-    if [ "$FASTDFS_MODE" = "storage" ]; then
+    if [ "$1" = "storage" ]; then
         PORT=23000
         # wait create storage data directory
         for ((i=0;i<=2;i++)); do
@@ -12,7 +12,7 @@ function status(){
                 sleep 3
             fi
         done
-    elif [ "$FASTDFS_MODE" = "tracker" ]; then
+    elif [ "$1" = "tracker" ]; then
         PORT=22122
     fi
 
@@ -29,10 +29,11 @@ function status(){
 # 容器运行中定时检查健康状态,异常则重启pod
 function monitor (){
     # tracker
-    if [ "$FASTDFS_MODE" == "tracker" ]; then
-        nc -vw 3 fastdfs-storage-svc -z 23000 >& /dev/null
+    if [ "$1" == "tracker" ]; then
+        nc -vw 3 localhost -z 23000 >& /dev/null
         if test $? -eq 0; then
             storage_num=$(fdfs_monitor /etc/fdfs/client.conf -h $(hostname -i) | grep 'ip_addr' | wc -l)
+            STORAGE_SERVER_NUM=$(tr_list=($TRACKERSERVER); echo ${#tr_list[@]})
             if [ $storage_num -gt $STORAGE_SERVER_NUM ]; then
                 echo '---> Tracker is down'
                 return 1
@@ -43,7 +44,7 @@ function monitor (){
     fi
 
     # starage
-    if [ "$FASTDFS_MODE" == "storage" ]; then
+    if [ "$1" == "storage" ]; then
         info=$(fdfs_monitor /etc/fdfs/storage.conf | grep $HOSTNAME | awk '{print $NF}')
         if [ "$info" == "ACTIVE" ]; then
             return 0
@@ -62,11 +63,11 @@ function monitor (){
     fi
 }
 
-case $1 in
+case $2 in
     monitor)
-        monitor ;;
+        monitor $1 ;;
     status)
-        status ;;
+        status $1 ;;
     *)
-        echo "Usage: $0 {status|monitor}" ;;
+        echo "Usage: $0 {storage|tracker} {status|monitor}" ;;
 esac
--- fdfs/start.sh
+++ fdfs/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 创建数据目录
-mkdir -p $STORAGE_DIR/data $TRACKER
+mkdir -p $STORAGE_DIR/data $TRACKER_DIR
 
 # 注释 TRACKER SERVER
 sed -i "/^tracker_server/d" /etc/fdfs/client.conf;
@@ -11,15 +11,34 @@ sed -i "/^tracker_server/d" /etc/fdfs/tracker.conf;
 # 配置 TRACKER SERVER
 TRACKERSERVER=${TRACKERSERVER:-localhost}
 for node in $TRACKERSERVER; do
-  echo "tracker_server=$node:$FDFS_PORT"  >> /etc/fdfs/client.conf
-  echo "tracker_server=$node:$FDFS_PORT"  >> /etc/fdfs/storage.conf
-  echo "tracker_server=$node:$FDFS_PORT"  >> /etc/fdfs/tracker.conf
+    echo "tracker_server=$node:$FDFS_PORT"  >> /etc/fdfs/client.conf
+    echo "tracker_server=$node:$FDFS_PORT"  >> /etc/fdfs/storage.conf
+    echo "tracker_server=$node:$FDFS_PORT"  >> /etc/fdfs/tracker.conf
 done
 
 # 启动服务
-echo '---> start trackerd'
-/etc/init.d/fdfs_trackerd start
+if [ "$1" == 'tracker' ]; then
+    echo '---> start trackerd'
+    /etc/init.d/fdfs_trackerd start
+    tail -f /dev/null
+elif [ "$1" == 'storage' ]; then
+    sleep 5
+    ARG=true
+    while $ARG; do
+        for node in $TRACKERSERVER; do
+            nc -vw 3 $node -z  22122 >& /dev/null
+            if [ $? -eq 0 ]; then
+                ARG=false
+            else
+                echo "---> wait $node port 22122 up"
+            fi
+            sleep 1
+        done
+    done
+    sleep 3
+    echo '---> start storage'
+    /etc/init.d/fdfs_storaged start
+    tail -f /dev/null
+fi
 
-sleep 5
-echo '---> start storage'
-/etc/init.d/fdfs_storaged start
+# /usr/local/nginx/sbin/nginx
