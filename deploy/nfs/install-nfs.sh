2192df8fce83f6dd50b011ee58c507f0f41bc25e
root <you@example.com>
2019-09-10 18:14:56 +0800

    update

--- deploy/nfs/install-nfs.sh
+++ deploy/nfs/install-nfs.sh
@@ -4,10 +4,10 @@
 yum install -y nfs-utils
 
 # 配置资源
-echo '/data/expample *(rw,sync,insecure,no_subtree_check,no_root_squash)' > /etc/exports
+echo '/data/example *(rw,sync,insecure,no_subtree_check,no_root_squash)' > /etc/exports
 
 # 创建数据目录
-mkdir -p /data/expample
+mkdir -p /data/example
 
 # 启动服务跟随系统启动
 systemctl start nfs
